c040cefb348125edd615324d61521f1c0260e07a
Pooja <pooja.14206@gmail.com>
2021-04-04 14:45:52 +0530

    [POOJA] modify and calculate the wages till a condition

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -7,11 +7,13 @@ public class EmpWageBuilderUC1 {
 		final int Part_Time = 1;
 		final int Full_Time = 2;
 		final int Wage_Per_Hr = 20;
-		final int Working_Days = 20;
+		final int Max_Working_Days = 20;
+		final int Max_Working_Hrs = 100;
 		int Total_Wage = 0;
 		int Working_Hr = 0;
 
-		for (int day = 1; day <= Working_Days; day++)
+		System.out.println("Day \t WorkingHours \t DailyEmployeeWage \t TotalWorkingHours");
+		for (int day = 1, Total_Working_Hrs = 0; day <= Max_Working_Days && Total_Working_Hrs < Max_Working_Hrs; day++, Total_Working_Hrs += Working_Hr)
 		{
 			int EmpCheck = (int) (Math.random() * 10) % 3;
 			switch (EmpCheck) {
@@ -27,12 +29,13 @@ public class EmpWageBuilderUC1 {
 					break;
 
 				default:
+					Working_Hr = 0;
 			}
 
 			int Daily_Employee_Wage = Wage_Per_Hr * Working_Hr;
 
-			System.out.println("Day" + day + "Wage is: " + Daily_Employee_Wage);
 			Total_Wage += Daily_Employee_Wage;
+			System.out.println( day + "\t" + Working_Hr + "\t"  +Daily_Employee_Wage + "\t" + Total_Working_Hrs);
 		}
 		System.out.println("Total Wage of a month: " + Total_Wage);
 
