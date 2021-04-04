22bea77a1b8d638e454df9ca51fe459f9ee1010f
Pooja <pooja.14206@gmail.com>
2021-04-04 10:36:56 +0530

    [POOJA] modify the file.

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -3,5 +3,17 @@ public class EmpWageBuilderUC1 {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+		int Check = 1;
+		double EmpCheck = Math.floor(Math.random() * 10) % 2;
+
+		if (EmpCheck == Check)
+
+			System.out.println("Enployee is present!");
+
+		else
+
+			System.out.println("Employee is Absent!");
+
 	}
 }
