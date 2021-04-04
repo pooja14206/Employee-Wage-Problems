70e4b6fa272f5c3dbe5fad3840c78fc0e5868e1c
Pooja <pooja.14206@gmail.com>
2021-04-04 11:46:14 +0530

    [POOJA] use Switch case

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -4,33 +4,32 @@ public class EmpWageBuilderUC1 {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		int Part_Time = 1;
-		int Full_Time = 2;
+		final int Part_Time = 1;
+		final int Full_Time = 2;
 		final int Wage_Per_Hr = 20;
 		int Working_Hr = 0;
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
+		int EmpCheck = (int) (Math.random() * 10) % 3;
 
-		if (EmpCheck == Part_Time)
-		{
 
-			System.out.println("Employee is present PartTime");
-			Working_Hr = 4;
+		switch (EmpCheck) {
+
+			case Full_Time:
+				System.out.println("Employee is present Full Time.");
+				Working_Hr = 8;
+				break;
+
+			case Part_Time:
+				System.out.println("Employee is Present Part Time.");
+				Working_Hr = 4;
+				break;
+
+			default:
+				System.out.println("Employee is Absent.");
+
 		}
-		else if (EmpCheck == Full_Time)
-		{
-			System.out.println("Employee is present FullTime");
-			Working_Hr = 8;
-
-		}
-		else
-
-			System.out.println("Employee is Absent!");
-
 		int Daily_Employee_Wage = Wage_Per_Hr * Working_Hr;
 
 		System.out.println("Daily EmployeeWage is " + Daily_Employee_Wage);
-
-
 
 	}
 }
