2093639e86bb95daa44df732c3f5a0a2d1f62a99
Pooja <pooja.14206@gmail.com>
2021-04-04 11:04:32 +0530

    [POOJA] add daily wage of Employee calculation

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -5,15 +5,25 @@ public class EmpWageBuilderUC1 {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		int Check = 1;
+		final int Wage_Per_Hr = 20;
+		int Working_Hr = 0;
 		double EmpCheck = Math.floor(Math.random() * 10) % 2;
 
 		if (EmpCheck == Check)
+		{
 
 			System.out.println("Enployee is present!");
-
+			Working_Hr = 8;
+		}
 		else
 
 			System.out.println("Employee is Absent!");
+
+		int Daily_Employee_Wage = Wage_Per_Hr * Working_Hr;
+
+		System.out.println("Daily EmployeeWage is " + Daily_Employee_Wage);
+
+
 
 	}
 }
