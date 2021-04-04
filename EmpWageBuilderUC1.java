1d9dddf9c544e14a3bb731a6f9f88b100bb12c26
Pooja <pooja.14206@gmail.com>
2021-04-04 11:27:36 +0530

    [POOJA] add Part Time Employee

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -4,16 +4,23 @@ public class EmpWageBuilderUC1 {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		int Check = 1;
+		int Part_Time = 1;
+		int Full_Time = 2;
 		final int Wage_Per_Hr = 20;
 		int Working_Hr = 0;
-		double EmpCheck = Math.floor(Math.random() * 10) % 2;
+		double EmpCheck = Math.floor(Math.random() * 10) % 3;
 
-		if (EmpCheck == Check)
+		if (EmpCheck == Part_Time)
 		{
 
-			System.out.println("Enployee is present!");
+			System.out.println("Employee is present PartTime");
+			Working_Hr = 4;
+		}
+		else if (EmpCheck == Full_Time)
+		{
+			System.out.println("Employee is present FullTime");
 			Working_Hr = 8;
+
 		}
 		else
 
